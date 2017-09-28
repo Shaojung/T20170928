11472dcbb15a0e5e6ac82d4f246fc90e2a8c5625
Shaojung Lu <shaojung@gmail.com>
2017-09-28 14:40:57 +0800

    change to get ItemId

--- app/src/main/java/com/example/teacher/t20170928/MainActivity.java
+++ app/src/main/java/com/example/teacher/t20170928/MainActivity.java
@@ -16,18 +16,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add("選單1");
-        menu.add("選單2");
+        menu.add(0, 1, 0, "選單一");
+        menu.add(0, 2, 0, "選單二");
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getTitle().equals("選單1"))
+
+        if (item.getItemId() == 1)
         {
             Toast.makeText(MainActivity.this, "選單一被按下", Toast.LENGTH_LONG).show();
         }
-        if (item.getTitle().equals("選單2"))
+        if (item.getItemId() == 2)
         {
             Toast.makeText(MainActivity.this, "選單二被按下", Toast.LENGTH_LONG).show();
         }
