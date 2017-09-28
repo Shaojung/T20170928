4e2d4d47567647cbfb4d030e68d67ab5687c9f74
Shaojung Lu <shaojung@gmail.com>
2017-09-28 14:58:38 +0800

    add menu res

--- app/src/main/java/com/example/teacher/t20170928/MainActivity.java
+++ app/src/main/java/com/example/teacher/t20170928/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.teacher.t20170928;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -16,8 +16,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0, 1, 0, "選單一");
-        menu.add(0, 2, 0, "選單二");
+
+//        menu.add(0, 1, 0, "選單一");
+//        menu.add(0, 2, 0, "選單二");
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -25,15 +28,22 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu1)
+        {
+            Toast.makeText(MainActivity.this, "新增被按下", Toast.LENGTH_LONG).show();
 
-        if (item.getItemId() == 1)
-        {
-            Toast.makeText(MainActivity.this, "選單一被按下", Toast.LENGTH_LONG).show();
         }
-        if (item.getItemId() == 2)
-        {
-            Toast.makeText(MainActivity.this, "選單二被按下", Toast.LENGTH_LONG).show();
-        }
+
+//        if (item.getItemId() == 1)
+//        {
+//            Toast.makeText(MainActivity.this, "選單一被按下", Toast.LENGTH_LONG).show();
+//        }
+//        if (item.getItemId() == 2)
+//        {
+//            Toast.makeText(MainActivity.this, "選單二被按下", Toast.LENGTH_LONG).show();
+//        }
+
+
         return super.onOptionsItemSelected(item);
     }
 }
